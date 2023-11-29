287e09cc433080657ce137301450af832ab29795
Moyudin Papu <moinpapu92446@gmail.com>
2023-11-30 03:36:43 +0530

    I have updated the digital clock code

--- clock.js
+++ clock.js
@@ -13,6 +13,18 @@ function updateClock() {
 
     hours = hours % 12;
 
+    if (hours < 10) {
+        hours = '0' + hours;
+    }
+    
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+    
+    if (seconds < 10) {
+        seconds = '0' + seconds;
+    } 
+
     var time = hours +`:`+ minutes +`:`+ seconds +" "+ ampm;
     document.getElementById("clock").textContent = time;
 }
